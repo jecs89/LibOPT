131cc41bb4ceff2602e8347ef0d75402a54276d3
Josimar <jecs89@gmail.com>
2017-12-01 16:19:11 -0200

    Experiments ABC Added

--- examples/ABC.c
+++ examples/ABC.c
@@ -7,21 +7,24 @@ int main(int argc, char const *argv[])
 	void* pFuncion;
 
     SearchSpace *s = NULL;
-    int i;
     printf("%d\n", atoi(argv[1]));
     pFuncion = param_function(pFuncion,atoi(argv[1]));
 
-    s = ReadSearchSpaceFromFile("../model_files/abc_model.txt", _ABC_); /* It reads the model file and creates a search space. We are going to use ABC to solve our problem. */
-
-    InitializeSearchSpace(s, _ABC_); /* It initalizes the search space */
-
-    if (CheckSearchSpace(s, _ABC_)) /* It checks wether the search space is valid or not */
+    int NE = 1;
+    for (int i = 0; i < NE; ++i)
     {
-    	
-        	runABC(s, pFuncion);          /* It minimizes function Sphere */
-    }
+	    s = ReadSearchSpaceFromFile("../model_files/abc_model.txt", _ABC_); /* It reads the model file and creates a search space. We are going to use ABC to solve our problem. */
 
-    DestroySearchSpace(&s, _ABC_); /* It deallocates the search space */
+	    InitializeSearchSpace(s, _ABC_); /* It initalizes the search space */
+
+	    if (CheckSearchSpace(s, _ABC_)) /* It checks wether the search space is valid or not */
+	    {
+	    	
+	        runABC(s, pFuncion);          /* It minimizes function Sphere */
+	    }
+
+	    DestroySearchSpace(&s, _ABC_); /* It deallocates the search space */
+	}
 
     return 0;
 }
